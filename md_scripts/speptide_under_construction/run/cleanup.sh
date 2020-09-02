0b381266985d3cbd4f68b1ad984c14db887e1044
Jacob North <microversallaboratories@gmail.com>
2020-09-02 08:29:22 -0700

    reorganize directories

--- md_scripts/speptide_under_construction/run/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm \#* *.trr *.tpr *.itp *.gro *.edr *.out mdout.mdp *.top
-rm *.xtc *.log *.cpt
-rm *.dat *GROMACS.o* *GROMACS.e*
--- md_scripts/water/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f mdout.mdp run_water.mdp
-rm -f \#* *.trr *.tpr *.itp *.gro *.edr *.out *.top
-rm -f *.ndx *.xtc *.log *.cpt *.xvg
-rm -f *.dat *GROMACS.o* *GROMACS.e*
